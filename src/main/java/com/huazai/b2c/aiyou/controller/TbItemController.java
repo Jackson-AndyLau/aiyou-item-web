f9c0e7bcc14da5e707312156664dacf651787cc0
huazai <who.seek.me@java98k.vip>
2019-12-15 16:46:01 +0800

    引入服务，新增实现商品详情展示

---
+++ src/main/java/com/huazai/b2c/aiyou/controller/TbItemController.java
@@ -0,0 +1,48 @@
+package com.huazai.b2c.aiyou.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.huazai.b2c.aiyou.pojo.TbItem;
+import com.huazai.b2c.aiyou.pojo.TbItemDesc;
+import com.huazai.b2c.aiyou.service.TbItemDescService;
+import com.huazai.b2c.aiyou.service.TbItemService;
+import com.huazai.b2c.aiyou.vo.TbItemVO;
+
+/**
+ * 
+ * @author HuaZai
+ * @contact who.seek.me@java98k.vip
+ *          <ul>
+ * @description 商品详情 Controller
+ *              </ul>
+ * @className TbItemController
+ * @package com.huazai.b2c.aiyou.controller
+ * @createdTime 2017年06月18日
+ *
+ * @version V1.0.0
+ */
+@RequestMapping("/item")
+public class TbItemController {
+
+	@Autowired
+	private TbItemService tbItemService;
+
+	@Autowired
+	private TbItemDescService tbItemDescService;
+
+	@RequestMapping("/{itemId}")
+	public String showTbItemInfo(@PathVariable Long itemId, Model model) {
+		// 通过商品Id，获取商品详情
+		TbItem tbItem = tbItemService.getTbItemById(itemId);
+		// 转换视图对象
+		TbItemVO item = new TbItemVO(tbItem);
+		// 通过商品Id，获取商品描述
+		TbItemDesc itemDesc = tbItemDescService.geTbItemDescById(itemId);
+		model.addAttribute("item", item);
+		model.addAttribute("itemDesc", itemDesc);
+		return "item";
+	}
+}

e0941e74c3546a5c64e93c7dbec8d980a573064a
huazai <who.seek.me@java98k.vip>
2019-12-15 16:08:51 +0800

    新增商品详情视图对象

---
+++ src/main/java/com/huazai/b2c/aiyou/vo/TbItemVO.java
@@ -0,0 +1,52 @@
+package com.huazai.b2c.aiyou.vo;
+
+import com.huazai.b2c.aiyou.pojo.TbItem;
+
+/**
+ * 
+ * @author HuaZai
+ * @contact who.seek.me@java98k.vip
+ *          <ul>
+ * @description 商品详情视图对象
+ *              </ul>
+ * @className TbItemVO
+ * @package com.huazai.b2c.aiyou.vo
+ * @createdTime 2017年06月18日
+ *
+ * @version V1.0.0
+ */
+public class TbItemVO extends TbItem {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public String[] getImages() {
+		String image = this.getImage();
+		if (image != null && !image.equals("")) {
+			String[] images = image.split(",");
+			return images;
+		}
+		return null;
+	}
+
+	public TbItemVO() {
+
+	}
+
+	public TbItemVO(TbItem tbItem) {
+		this.setBarcode(tbItem.getBarcode());
+		this.setCid(tbItem.getCid());
+		this.setCreated(tbItem.getCreated());
+		this.setId(tbItem.getId());
+		this.setImage(tbItem.getImage());
+		this.setNum(tbItem.getNum());
+		this.setPrice(tbItem.getPrice());
+		this.setSellPoint(tbItem.getSellPoint());
+		this.setStatus(tbItem.getStatus());
+		this.setTitle(tbItem.getTitle());
+		this.setUpdated(tbItem.getUpdated());
+	}
+
+}
